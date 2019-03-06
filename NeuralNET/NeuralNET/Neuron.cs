43f2956d04fa306e3f674c3cf0b9b506e238874d
Marek Pavelka <marek.pavelka@gmail.com>
2019-03-06 17:32:13 +0100

    Neural

---
+++ NeuralNET/NeuralNET/Neuron.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NeuralNET
+{
+    class Neuron
+    {
+        private double[] _weights;
+        private double _bias;
+
+        public Neuron(int inputCount)
+        {
+            _weights = new double[inputCount];
+            Random generator = new Random();
+            for (int index = 0; index < _weights.Length; index++)
+            {
+                _weights[index] = generator.NextDouble();
+            }
+
+            _bias = generator.NextDouble();
+        }
+
+        public double Evaluate(double[] inputs)
+        {
+            var inputSum = 0.0;
+            for (var i = 0; i < inputs.Length; i++)
+            {
+                inputSum = inputSum + inputs[i] * _weights[i];
+            }
+            var result = ActivationFunc(inputSum + _bias);
+            return result;
+        }
+
+        private double ActivationFunc(double input)
+        {
+            return 1 / (1 + Math.Exp(-input));
+        }
+    }
+}
---
+++ NeuralNET/NeuralNET/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNET
+{
+    class Program
+    {
+        static void Main()
+        {
+            var newNeuron = new Neuron(8);
+            var result = newNeuron.Evaluate(new double[8] { 1, 2, 3, 4, 5, 6, 7, 8 });
+
+        }
+    }
+
+    class NeuralNetwork
+    {
+        private Neuron[] _inputNeurons;
+        private Neuron[] _hiddeNeurons;
+        private Neuron[] _outputNeurons;
+
+        public NeuralNetwork(int inputNumber, int hiddenNumber, int outputNumber)
+        {
+            _inputNeurons = new Neuron[inputNumber];
+            for (int i = 0; i < _inputNeurons.Length; i++)
+            {
+                _inputNeurons[i] = new Neuron(1);
+            }
+            _hiddeNeurons = new Neuron[hiddenNumber];
+            for (int i = 0; i < _hiddeNeurons.Length; i++)
+            {
+                _hiddeNeurons[i] = new Neuron(inputNumber);
+            }
+            _outputNeurons = new Neuron[outputNumber];
+            for (int i = 0; i < _outputNeurons.Length; i++)
+            {
+                _outputNeurons[i] = new Neuron(hiddenNumber);
+            }
+        }
+
+        public double[] EvaluteNetwork(double[] inputs)
+        {
+            var inputNeuronsResults = new double[_inputNeurons.Length];
+            for (int i = 0; i < _inputNeurons.Length; i++)
+            {
+                var neuron = _inputNeurons[i];
+                var input = inputs[i];
+                inputNeuronsResults[i] = neuron.Evaluate(input);
+            }
+        }
+
+
+    }
+}
